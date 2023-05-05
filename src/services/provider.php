7a798a14a0d065b89747c6c212a5f5b9126c1574
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2023-05-05 23:30:07 +0200

    Add copyright block for JED

--- src/services/provider.php
+++ src/services/provider.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * @package plugin.system hyphenateghsvs for Joomla!
+ * @version See hyphenateghsvs.xml
+ * @author G@HService Berlin Neukölln, Volkmar Volli Schlothauer
+ * @copyright Copyright (C) 2016-2020, G@HService Berlin Neukölln, Volkmar Volli Schlothauer. All rights reserved.
+ * @license GNU General Public License version 3 or later; see LICENSE.txt; see also LICENSE_Hyphenopoly.txt
+ * @authorUrl https://www.ghsvs.de
+ * @link https://github.com/GHSVS-de/plg_system_hyphenateghsvs_slim
+ */
+
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Extension\PluginInterface;
